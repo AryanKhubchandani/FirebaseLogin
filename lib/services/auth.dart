341871b6a7a5942378a632924661c9847fa45d8b
Aryan <aryan.khubchandani@gmail.com>
2021-05-22 13:56:11 +0530

    add Firesbase auth

---
+++ lib/services/auth.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in with email and password
+
+  // sign in using google
+
+  // sign in using OTP
+
+  // register using email and password
+
+  // register using google
+
+  // register using OTP
+
+  // sign out
+
+}
