20ff67d22304b0b969bd81c216a4ef105d7f5269
Aryan <aryan.khubchandani@gmail.com>
2021-05-22 21:57:02 +0530

    return uid only

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_login/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // User Object based on FirebaseUser
+  Users _fbUser(User user) {
+    return user != null ? Users(uid: user.uid) : null;
+  }
 
   // sign in anon
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User user = result.user;
-      return user;
+      return _fbUser(user);
     } catch (e) {
       print(e.toString());
       return null;
