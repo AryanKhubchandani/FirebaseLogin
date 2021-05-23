de4e89cf3e939974b44e86318fed9ddba04f02f7
Aryan <aryan.khubchandani@gmail.com>
2021-05-24 02:46:16 +0530

    add validator

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -10,6 +10,8 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
   String email = '';
   String password = '';
 
@@ -34,10 +36,12 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 40.0),
               TextFormField(
+                validator: (val) => val.isEmpty ? "Enter an email" : null,
                 onChanged: (val) {
                   setState(() => email = val);
                 },
@@ -45,6 +49,9 @@ class _RegisterState extends State<Register> {
               SizedBox(height: 20.0),
               TextFormField(
                 obscureText: true,
+                validator: (val) => val.length < 6
+                    ? "Enter a password habing 6+ characters"
+                    : null,
                 onChanged: (val) {
                   setState(() => password = val);
                 },
@@ -56,8 +63,10 @@ class _RegisterState extends State<Register> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate()) {
+                    print(email);
+                    print(password);
+                  }
                 },
               )
             ],
